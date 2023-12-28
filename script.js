520fcccb628502051145048c83ef7215a53706e2
Dhawal Bisht <dhawalbisht4543@gmail.com>
2023-12-28 18:51:02 +0530

    animation added

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ function generateQr(){
     let imgBox = document.getElementById("imgBox");
     let qrText = document.getElementById("qrText"); 
     const qrImage = document.getElementById("qrImage");
-    if(qrText.value){
+    if(qrText.value.length>0){
         qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        imgBox.classList.add("show-img")
     }
     else{
-        qrImage.remove()
+        qrText.classList.add('error');
+        setTimeout(()=>{
+            qrText.classList.remove('error')
+        },1000);
     }
 }
